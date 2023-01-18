35541029f81456c8f71c314172a4e13e3764412b
Alessandro-Villani <alessvill@libero.it>
2023-01-18 16:28:02 +0100

    added email list and loader

--- js/script.js
+++ js/script.js
@@ -3,9 +3,37 @@ console.log('VUE OK', Vue);
 const app = Vue.createApp({
     data(){
         return {
-
+            emailList: [],
+            isLoading: false
         }
+    },
+    computed:{
+        emailListLength(){
+            return this.emailList.length;
+        }
+    },
+    methods:{
+        getEmailAddresses(number){
+            this.isLoading = true;
+            for(i=0; i < number; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( response => {
+                    const email = response.data.response;
+                    this.emailList.push(email);
+                }).catch(error => {}).then(() => {
+                    const wait = setInterval(() => {
+                        if(this.emailList.length === number){
+                            this.isLoading = false;
+                            clearInterval(wait);
+                        }
+                    }, 10)
+                })
+            }
+        }
+    },
+    mounted(){
+        this.getEmailAddresses(10);
     }
+
 });
 
 app.mount('#root');
